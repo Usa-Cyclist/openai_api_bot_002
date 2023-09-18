66d62bb3daeb79911e4dd1ca1b102de47e946d34
Usa-Cyclist <145240885+Usa-Cyclist@users.noreply.github.com>
2023-09-18 12:37:13 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+
+import streamlit as st
+import openai
+
+# Streamlit Community Cloudの「Secrets」からOpenAI API keyを取得
+openai.api_key = st.secrets.OpenAIAPI.openai_api_key
+
+# st.session_stateを使いメッセージのやりとりを保存
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [
+        {"role": "system", "content": "あなたはおえかきManakaが作った優秀なアシスタントAIです。"}
+        ]
+
+# チャットボットとやりとりする関数
+def communicate():
+    messages = st.session_state["messages"]
+
+    user_message = {"role": "user", "content": st.session_state["user_input"]}
+    messages.append(user_message)
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )  
+
+    bot_message = response["choices"][0]["message"]
+    messages.append(bot_message)
+
+    st.session_state["user_input"] = ""  # 入力欄を消去
+
+
+# ユーザーインターフェイスの構築
+st.title("おえかきManakaのAI アシスタント")
+st.write("プログラミングを学んで、AIチャットボットを作りました")
+
+user_input = st.text_input("メッセージを入れてね", key="user_input", on_change=communicate)
+
+if st.session_state["messages"]:
+    messages = st.session_state["messages"]
+
+    for message in reversed(messages[1:]):  # 直近のメッセージを上に
+        speaker = "😃"
+        if message["role"]=="assistant":
+            speaker="🤖"
+
+        st.write(speaker + ": " + message["content"])
